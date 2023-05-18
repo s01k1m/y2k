bf9b8b37476bc07e60b588d8799ed7f11a802564
flowerdonk <dbwls5140@gmail.com>
2023-05-18 12:34:51 +0900

    search bar

--- final_pjt_front/src/router/index.js
+++ final_pjt_front/src/router/index.js
@@ -1,15 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-<<<<<<< HEAD
+
 import Home from '../views/HomeView.vue'
 import Search from '../views/SearchView.vue'
-=======
-
-
-import Login from '../views/LoginView.vue'
-import Home from '../views/HomeView.vue'
-// import Still from '../views/StillsView.vue'
->>>>>>> 85da6e359ae9438945b0ff39679f1196c81bddcc
 import Create from '../views/CreateView.vue'
 import Login from '../views/LoginView.vue'
 import SignUp from '../views/SignUpView.vue'
