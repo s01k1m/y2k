2f9800b5f4789685c958ec6f4915d3de2af281f6
flowerdonk <dbwls5140@gmail.com>
2023-05-18 17:53:51 +0900

    게시글 생성 nav 클릭 시, 로그인 여부 확인

--- final_pjt_front/src/views/CreateView.vue
+++ final_pjt_front/src/views/CreateView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="create">
     <h1>스틸컷을 크리에이트하는 포스팅 페이지입니다. </h1>
-    
   </div>
 </template>
 <script>
@@ -12,6 +11,23 @@ export default {
       title: null,
       image: null,
       movie_id: null,
+    }
+  },
+  computed: {
+    isLogin() {
+      return this.$store.state.isLogin
+    }
+  },
+  created() {
+    this.isLogined()
+  },
+  methods: {
+    isLogined() {
+      if (!this.isLogin) {
+        console.log('if')
+        alert('로그인이 필요합니다.')
+        this.$router.push({ name: 'login' })
+      } 
     }
   }
 }
