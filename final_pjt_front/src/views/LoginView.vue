705aaa05d75f51c555a61c3ba428e9ce43146db6
s01k1m <sk618dev@gmail.com>
2023-05-18 19:59:46 +0900

    (vue)로그인 css

--- final_pjt_front/src/views/LoginView.vue
+++ final_pjt_front/src/views/LoginView.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="login">
-    <h1>로그인</h1>
-    <label for="username">username : </label>
-    <input type="text" id="username" v-model="username"><br>
-    <label for="password"> password : </label>
-    <input type="password" id="password" v-model="password"><br>
-    <button v-on:click="loginSubmit">Log in</button><br>
-    <router-link :to="{ name: 'signup' }">아직 가입하지 않으셨나요?</router-link>
-    <!-- <button type="submit" v-on:click="Signin">Sign in</button> -->
+    <h1>STILL에 오신 것을 환영합니다</h1>
+    <div class="loginform">
+
+      <label for="username">유저이름</label><br>
+      <input type="text" id="username" v-model="username"><br>
+      <label for="password">비밀번호</label><br>
+      <input type="password" id="password" v-model="password"><br>
+      <button v-on:click="loginSubmit">Log in</button><br>
+    </div>
+    <div class='router' >
+      
+      <router-link :to="{ name: 'signup' }">아직 STILL을 사용하고 있지 않으신가요? 가입하기</router-link>
+    </div>
+      <!-- <button type="submit" v-on:click="Signin">Sign in</button> -->
   </div>  
 </template>
 
@@ -39,6 +45,77 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+.login {
+  
+}
+
+h1 {
+    color: rgb(51, 51, 51);
+    font-size: 32px;
+    font-weight: 600;
+    letter-spacing: -1.2px;
+    padding-left: 16px;
+    padding-right: 16px;
+    -webkit-font-smoothing: antialiased;
+    word-break: keep-all;
+}
+
+.loginform label {
+  left: 0;
+  font-size: 14px;
+  color: #111111;
+}
+
+ .loginform{
+  width: 268px;
+  margin: 0 auto;
+  text-align: left;
+}
+
+.router {
+  width: 268px;
+  margin: 0 auto;
+  text-align: center;
+  border: none;
+  padding: 0px;
+  display: block;
+  color: rgb(51, 51, 51);
+  font-weight: bold;
+}
+.router > a{
+  font-size: 12px;
+  text-decoration: none;
+  color: inherit;
+}
+
+.loginform > input{
+  width: 100%;
+  border-color: #cdcdcd;
+  min-height: 48px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-radius: 16px;
+  border-style: solid;
+  border-width: 2px;
+  padding: 8px 16px;
+}
+
+.loginform > button {
+  margin: 0 auto;
+  width: 100%;
+  margin: 30px 0px;
+  background-color: rgb(0, 0, 0);
+  font-size: 15px;
+  font-weight: bold;
+  border-radius: 20px;
+  padding: 0px 18px;
+  border: 0px;
+  height: 40px;
+  vertical-align: middle;
+  color: white;
+}
+
 
 </style>
