fdc68b8524a22508a23dcf385ef6b45db80b03e3
flowerdonk <dbwls5140@gmail.com>
2023-05-21 21:36:16 +0900

    home화면 스틸컷 컬러별로 분류해서 보이기 완성(카테고리 X)

--- final_pjt_back/stills/views.py
+++ final_pjt_back/stills/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from .models import Still
 from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework.response import Response
+from .serializers import StillSerializer
+from django.http import JsonResponse
 
-def still_list(request):
-    still_list = get_list_or_404(Still)
-    print(still_list)
-    return Response(status=200)
-
-def still_of_color(request, colorChoice):
-    print('still_of_color 진입')
-    still_of_color_list = get_list_or_404(Still, still_color=colorChoice)
-    print('still_of_color_list: ', still_of_color_list)
-    return Response(status=200)
+def still_list(request, colorChoice):
+    print(colorChoice)
+    if colorChoice == 'ALL':
+        print('still_list 진입')
+        still_list = get_list_or_404(Still)
+        print('still_list: ', still_list)
+        serializer = StillSerializer(still_list, many=True)
+        print('serializer.data : ', serializer.data)
+    else:
+        print('still_of_color 진입')
+        still_of_color_list = get_list_or_404(Still, still_color=colorChoice)
+        print('still_of_color_list: ', still_of_color_list)
+        serializer = StillSerializer(still_of_color_list, many=True)
+        print('serializer.data : ', serializer.data)
+        # 만약 일치하는 데이터가 없다면 []가 반환된다.
+    return JsonResponse(serializer.data, safe=False)
--- final_pjt_front/src/views/HomeView.vue
+++ final_pjt_front/src/views/HomeView.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <h1>이 페이지는 홈이면서, 스틸컷의 나열 페이지입니다.</h1>
     <h1>{{ colorChoice }}</h1>
+    <div v-for="(still, index) in stillBasedOnColor" :key="index">
+      {{ still }}
+    </div>
     <StillCard></StillCard>
   </div>
 </template>
@@ -15,18 +18,33 @@ export default {
   components: {
     StillCard
   },
+  data() {
+    return {
+      colorChoice : null,
+      stillBasedOnColor : [],
+    }
+  },
   created() {
+    if (!this.colorChoiceFromIntro) { // 선택된 색상이 없을 때 -> 인트로를 거치지 않고, 주소로 바로 들어왔을 때
+      this.colorChoice = 'ALL' // 전부 보여주기
+      console.log('선택 색상이 없음!!')
+    } else {
+      this.colorChoice = this.colorChoiceFromIntro
+    }
+    console.log('HomeView created 진입!')
+    console.log(this.colorChoice)
+
     axios
       .get(`http://127.0.0.1:8000/stills/${this.colorChoice}/`) // django에서 db에 저장된 해당 색상 stillcut 정보를 받아옴
       .then((response) => {
-        console.log("response.data", response.data);
+        this.stillBasedOnColor = response.data
       })
       .catch((error) => {
         console.error(error);
       });
   },
   computed: {
-    colorChoice() {
+    colorChoiceFromIntro() {
       return this.$route.params.colorChoice
     }
   },
