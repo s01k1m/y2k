d1849401274b5c4ea6ce70cfd2bf8d86be3bdb14
s01k1m <sk618dev@gmail.com>
2023-05-24 11:51:27 +0900

    (순정) dumpdata basic.json

--- final_pjt_back/stills/admin.py
+++ final_pjt_back/stills/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Movie
 from .models import Still
-
+from .models import Collection
 
 admin.site.register(Movie)
 admin.site.register(Still)
+admin.site.register(Collection)
